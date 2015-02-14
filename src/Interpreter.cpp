344b696925119b5deab21adb014dad594bc4b16e
Jón Freysteinn Jónsson <nonni95@gmail.com>
2015-02-14 11:12:22 +0000

    refactoring

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -11,71 +11,50 @@ void error_msg (string msg) {
 }
 
 int main () {
-    map<string, int> mp;
-    stack<string> stk;
-    string in, ID;
+    map<string, int> mp; // Map to hold our variables
+    stack<string> stk; // Stack to hold values and variables
+    string in, ID; 
     int VALUE, a(0), b(0);
-    
+
     while (cin >> in) {
         if(in == "PUSH") {
             if(cin >> in) {
                 stk.push(in);
             } else {
-                error_msg("Missing argument!"); 
+                error_msg("Missing argument!\n"); 
             }
         } else if (in == "PRINT") {
             if(mp.count(stk.top())) {
                 cout << mp[stk.top()] << endl;
             } else {
-                cout << stk.top();
+                error_msg("No such variable!\n");
             }
             stk.pop();
-        } else if (in == "ADD") {
+        } else if (in == "ADD" || in == "SUB" || in == "MULT") {
+            // Check if variable in map, else use as value
             if(mp.count(stk.top())) {
                 a = mp[stk.top()];
             } else {
                 a = stoi(stk.top());
             }
             stk.pop();
+            // Check if variable in map, else use as value
             if(mp.count(stk.top())) {
                 b = mp[stk.top()];
             } else {
                 b = stoi(stk.top());
             }
             stk.pop();
-            b += a;
-            stk.push(std::to_string((long long)b));
-        } else if (in == "SUB") {
-            if(mp.count(stk.top())) {
-                a = mp[stk.top()];
+            if(in == "ADD") {
+                b += a;
+            } else if (in == "SUB") {
+                b -= a;
             } else {
-                a = stoi(stk.top());
+                b *= a;
             }
-            stk.pop();
-            if(mp.count(stk.top())) {
-                b = mp[stk.top()];
-            } else {
-                b = stoi(stk.top());
-            }
-            stk.pop();
-            b -= a;
-            stk.push(to_string((long long)b));
-        } else if (in == "MULT") {
-            if(mp.count(stk.top())) {
-                a = mp[stk.top()];
-            } else {
-                a = stoi(stk.top());
-            }
-            stk.pop();
-            if(mp.count(stk.top())) {
-                b = mp[stk.top()];
-            } else {
-                b = stoi(stk.top());
-            }
-            stk.pop();
-            b *= a;
             stk.push(to_string((long long)b));
         } else if (in == "ASSIGN") {
+            // Check if variable in map, else use as value
             if(mp.count(stk.top())) {
                 VALUE = mp[stk.top()];
             } else {
@@ -86,9 +65,7 @@ int main () {
             stk.pop();
             mp.insert(make_pair(ID, VALUE));
         } else {
-            error_msg("Wrong command!");
+            error_msg("Wrong command!\n"); // Input was not defined
         }
     }
 }
-
-
