e12e3aebc9cc655584bf0aea09f384fd5bff321c
Jón Freysteinn Jónsson <nonni95@gmail.com>
2015-02-14 12:06:38 +0000

    MORE REFACTORING git add .! MUHAHAHA

--- src/Interpreter.cpp
+++ src/Interpreter.cpp
@@ -5,12 +5,15 @@
 
 using namespace std;
 
+map<string, int> mp;
+stack<string> stk;
+
 void error_msg (string msg) {
-    cout << "Error: " << msg << endl;
+    cout << "Error for operator: " << msg << endl;
     exit(0);
 }
 
-int get_value (map<string, int> mp, stack<string> stk) {
+int get_value () {
     if(mp.count(stk.top())) {
         return mp[stk.top()];
     } else {
@@ -19,50 +22,42 @@ int get_value (map<string, int> mp, stack<string> stk) {
 }
 
 int main () {
-    map<string, int> mp;
-    stack<string> stk;
     string in, ID;
     int VALUE, a(0), b(0);
     
     while (cin >> in) {
         if(in == "PUSH") {
-            if(cin >> in) {
+            if(cin >> in)
                 stk.push(in);
-            } else {
-                error_msg("Missing argument!"); 
-            }
+            else
+                error_msg("PUSH, missing argument!"); 
         } else if (in == "PRINT") {
-            cout << get_value(mp, stk) << endl;
+            if(stk.empty())
+                error_msg(in + ", not enough values on stack!");
+            cout << get_value() << endl;
+        } else if (in == "ADD" || in =="SUB" || in == "MULT") {
+            if(stk.size() < 2)
+                error_msg(in + ", not enough values on stack!");
+            a = get_value();
             stk.pop();
-        } else if (in == "ADD") {
-            a = get_value(mp, stk);
+            b = get_value();
             stk.pop();
-            b = get_value(mp, stk);
-            stk.pop();
-            b += a;
+            if(in == "ADD")
+                b += a;
+            else if(in == "SUB")
+                b -= a;
+            else
+                b *= a;
             stk.push(std::to_string((long long)b));
-        } else if (in == "SUB") {
-            a = get_value(mp, stk);
-            stk.pop();
-            b = get_value(mp, stk);
-            stk.pop();
-            b -= a;
-            stk.push(to_string((long long)b));
-        } else if (in == "MULT") {
-            a = get_value(mp, stk);
-            stk.pop();
-            b = get_value(mp, stk);
-            stk.pop();
-            b *= a;
-            stk.push(to_string((long long)b));
         } else if (in == "ASSIGN") {
-            VALUE = get_value(mp, stk);
+            if(stk.size() < 2)
+                error_msg(in + ", not enough values on stack!");
+            VALUE = get_value();
             stk.pop();
             ID = stk.top();
             stk.pop();
             mp.insert(make_pair(ID, VALUE));
-        } else {
-            error_msg("Wrong command!");
-        }
+        } else
+            error_msg(in);
     }
 }
